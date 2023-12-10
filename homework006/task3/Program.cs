d0960a9567b3dda29d1737379568c4f3cdb0dac3
Dima Bely <deem4an@gmail.com>
2023-12-10 20:56:14 +0300

    Task3 complete!

---
+++ homework006/task3/Program.cs
@@ -0,0 +1,16 @@
+﻿class Task3 {
+    public static void Main(string[] args) {
+        string example = "Топот";
+        Console.WriteLine(IsPalindrome(example));
+    }
+
+    static bool IsPalindrome(string str) {
+        char[] charArrayReverse = str.ToLower().ToCharArray();
+        Array.Reverse(charArrayReverse);
+        string stringRevers = new string(charArrayReverse);
+        Console.WriteLine(stringRevers);
+        if (str.ToLower().Equals(stringRevers)) {
+            return true;
+        } else return false;
+    }
+}
