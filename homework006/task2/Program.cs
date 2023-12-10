19ea8b818e1d4c0e56afe43d9edf433d087011b6
Dima Bely <deem4an@gmail.com>
2023-12-10 20:21:35 +0300

    Task2 complete!

---
+++ homework006/task2/Program.cs
@@ -0,0 +1,9 @@
+﻿class Task2 {
+    public static void Main(string [] args) {
+        string upperCase = "AbcdEFgHjKlmnoPqRsTuVWxyZ";
+        Console.WriteLine(StringToLowerCase(upperCase));
+    }
+    static string StringToLowerCase (string str) {
+        return str.ToLower();
+    }
+}
