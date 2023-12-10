676bb4ef09c2b63178697657c4ccb949e984e433
Dima Bely <deem4an@gmail.com>
2023-12-10 20:10:06 +0300

    Task1 complete!

---
+++ homework006/task1/Program.cs
@@ -0,0 +1,27 @@
+﻿class Program
+{
+    public static void Main(string[] args)
+    {
+
+        char[,] chars = {
+        {'a', 'b', 'c'},
+        {'d', 'e', 'f'},
+        {'g', 'h', 'i'}
+                        };
+
+        Console.WriteLine(CreateString(chars));
+    }
+
+    static string CreateString(char[,] chars)
+    {
+        string result = string.Empty;
+        for (int i = 0; i < chars.GetLength(0); i++)
+        {
+            for (int j = 0; j < chars.GetLength(1); j++)
+            {
+                result += chars[i,j];
+            }
+        }
+        return result;
+    }
+}
